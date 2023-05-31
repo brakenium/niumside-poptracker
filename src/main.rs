15ab0f11a82b05491e86ac9155c704144ccedf29
brakenium <brakenium@example.com>
2023-05-31 17:35:55 +0200

    Initial shuttle

--- src/main.rs
+++ src/main.rs
@@ -3,47 +3,35 @@
 #![warn(clippy::unwrap_used)]
 #![warn(clippy::expect_used)]
 
-mod configuration;
 mod constants;
 mod realtime;
 mod event_handlers;
 mod active_players;
-mod storage;
 mod logging;
+mod shuttle;
+
 use futures::future;
 use std::collections::HashMap;
 use std::error::Error;
 use std::sync::{Arc, Mutex};
+use sqlx::{Executor, PgPool};
+use shuttle_runtime::CustomError;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let app_config = configuration::Settings::new()?;
+#[shuttle_runtime::main]
+async fn init(
+    #[shuttle_shared_db::Postgres] postgres: PgPool,
+    #[shuttle_secrets::Secrets] secrets: shuttle_secrets::SecretStore,
+) -> Result<shuttle::NiumsideService, shuttle_runtime::Error> {
+    postgres.execute(include_str!("../niumside-database.sql"))
+        .await
+        .map_err(CustomError::new)?;
 
-    logging::init(app_config.app.log_level);
-
-    // write a match expression for realtime::init(app_config.census, app_config.worlds).await
-    // if events is Ok, then do the following
-    // if events is Err, then do the following
-    let events = match realtime::init(app_config.census, app_config.worlds).await {
-        Ok(events) => events,
-        Err(e) => {
-            panic!("Unable to connect to realtime API: {e}");
-        }
-    };
-
-    let Ok(db_pool) = storage::pool::create(&app_config.database.connection_string).await
-    else {
-        panic!("Unable to connect to database");
-    };
 
     let active_players: active_players::ActivePlayerDb = Arc::new(Mutex::new(HashMap::new()));
 
-    let futures = vec![
-        tokio::spawn(active_players::process_loop(active_players.clone(), db_pool)),
-        tokio::spawn(event_handlers::receive_events(events, active_players.clone())),
-        tokio::spawn(active_players::clean(active_players.clone()))
-    ];
-
-    future::join_all(futures).await;
-    Ok(())
+    Ok(shuttle::NiumsideService {
+        active_players,
+        db_pool: postgres,
+        secrets,
+    })
 }
--- src/realtime.rs
+++ src/realtime.rs
@@ -1,4 +1,4 @@
-use auraxis::AuraxisError;
+use auraxis::{AuraxisError, WorldID};
 use auraxis::realtime::client::{RealtimeClient, RealtimeClientConfig};
 use auraxis::realtime::event::{Event, EventNames};
 use auraxis::realtime::subscription::{
@@ -6,21 +6,17 @@ use auraxis::realtime::subscription::{
 };
 use tokio::sync::mpsc::Receiver;
 
-use crate::configuration::{self, WorldConfig};
-
-pub async fn init(census_config: configuration::CensusConfig, worlds: Vec<WorldConfig>) -> Result<Receiver<Event>, AuraxisError> {
+pub async fn init(secrets: shuttle_secrets::SecretStore) -> Result<Receiver<Event>, AuraxisError> {
     let realtime_config = RealtimeClientConfig {
-        service_id: census_config.service_id,
-        realtime_url: Some(census_config.realtime_base_url.to_string()),
+        service_id: secrets.get("SERVICE_ID").unwrap(),
+        realtime_url: secrets.get("REALTIME_URL"),
         ..RealtimeClientConfig::default()
     };
-
-    let worlds = worlds.iter().map(|value| value.id).collect();
 
     let subscription = SubscriptionSettings {
         event_names: Some(EventSubscription::Ids(vec![EventNames::GainExperience])),
         characters: Some(CharacterSubscription::All),
-        worlds: Some(WorldSubscription::Ids(worlds)),
+        worlds: Some(WorldSubscription::All),
         logical_and_characters_with_worlds: Some(true),
         ..SubscriptionSettings::default()
     };
