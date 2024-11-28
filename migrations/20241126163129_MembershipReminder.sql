b9d657c3634d8a320d1fdc33f2c9e5fe763e7335
brakenium <brake@brakenium.xyz>
2024-11-28 19:39:30 +0100

    Use CREATE TABLE IF NOT EXISTS

---
+++ migrations/20241126163129_MembershipReminder.sql
@@ -0,0 +1,28 @@
+-- Add migration script here
+BEGIN;
+
+CREATE TABLE public.users
+(
+    user_id    SERIAL NOT NULL,
+    discord_id BIGINT
+        CONSTRAINT "CH_users_discord_id" CHECK (is_i64(discord_id) AND is_u64(discord_id)),
+    CONSTRAINT "PK_users" PRIMARY KEY (user_id),
+    CONSTRAINT "UK_users_discord_id" UNIQUE (discord_id)
+);
+
+CREATE TABLE public.planetside_characters
+(
+    character_id             BIGINT   NOT NULL,
+    user_id                  SERIAL   NOT NULL,
+    name                     TEXT     NOT NULL,
+    faction_id               SMALLINT NOT NULL,
+    membership_reminder      BOOLEAN  NOT NULL DEFAULT FALSE,
+    last_membership_reminder TIMESTAMP WITHOUT TIME ZONE,
+    last_fetch               TIMESTAMP WITHOUT TIME ZONE,
+    CONSTRAINT "PK_planetside_characters" PRIMARY KEY (character_id),
+    CONSTRAINT "UK_planetside_characters_discord_id" UNIQUE (user_id, character_id),
+    CONSTRAINT "FK_planetside_characters_users" FOREIGN KEY (user_id)
+        REFERENCES public.users (user_id)
+);
+
+COMMIT;
